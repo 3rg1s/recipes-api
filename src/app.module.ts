d586fd726657b4bab968ef02ee83d4f7244ed7e9
fuxsocy.py <0x0byte@protonmail.com>
2021-05-28 09:29:14 +0300

    Added postgresql database connection.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,27 @@
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RecipesModule } from './recipes/recipes.module';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, RecipesModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(<string>process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_NAME,
+      autoLoadEntities: true,
+      synchronize: true, // should not be used on production env
+    }),
+    UsersModule,
+    RecipesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
