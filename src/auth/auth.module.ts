330ea928cbaeb831964da40c44a16d2c25c0e770
fuxsocy.py <0x0byte@protonmail.com>
2021-05-28 19:43:49 +0300

    Added use guard on endpoint recipe/all. This will ensure only authenticated users will have the ability to view recipes

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './services/auth/auth.service';
 import { JwtModule } from '@nestjs/jwt';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { JwtAuthGuard } from './strategies/guards/jwt-auth.guard';
 
--- src/users/controller/users.controller.ts
+++ src/users/controller/users.controller.ts
@@ -1,7 +1,9 @@
 import {
   Body,
   Controller,
+  Get,
   Post,
+  UseGuards,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
@@ -10,6 +12,9 @@ import { UserSignupDto } from '../dtos/usersignup.dto';
 import { UserLoginDto } from '../dtos/userlogin.dto';
 import { UserInterface } from '../models/user.interface';
 import { IsEmail } from 'class-validator';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { JwtAuthGuard } from 'src/auth/strategies/guards/jwt-auth.guard';
 
 @Controller('users')
 export class UsersController {
@@ -24,6 +29,10 @@ export class UsersController {
   @Post('login')
   @UsePipes(ValidationPipe)
   async loginuser(@Body() userLoginDto: UserLoginDto) {
-    return this.UserService.login(userLoginDto);
+    return this.UserService.login(userLoginDto).then((result) => {
+      return {
+        access_token: result,
+      };
+    });
   }
 }
