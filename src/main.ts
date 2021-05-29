c58c0624ffc49743a74ceda2431f981388a7dda1
fuxsocy.py <0x0byte@protonmail.com>
2021-05-30 00:58:05 +0300

    Renamed a user controller endpoint, and changed http status code on validation pipes

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { ValidationPipe } from '@nestjs/common';
+import { HttpStatus, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
@@ -12,7 +12,11 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+    }),
+  );
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
